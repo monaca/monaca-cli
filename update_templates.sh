26d18ec2727e46a155d79bea60d374939cfb6135
Andreas Argelius <andreas@argeli.us>
2015-04-17 14:51:45 +0900

    Updated templates to 1.3.0.

---
+++ update_templates.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget http://onsen.io/project-templates/gen/onsenui-master-detail.zip -O templates/onsen_master_detail.zip
+wget http://onsen.io/project-templates/gen/onsenui-sliding-menu.zip -O templates/onsen_sliding_menu.zip
+wget http://onsen.io/project-templates/gen/onsenui-tab-bar.zip -O templates/onsen_tab_bar.zip
+wget http://onsen.io/project-templates/gen/onsenui-split-view.zip -O templates/onsen_split_view.zip
