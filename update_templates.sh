aebbacb98d80350592c758cb272cd095323a0f6f
Andreas Argelius <andreas@argeli.us>
2015-04-17 14:59:31 +0900

    Fixed broken templates.

--- update_templates.sh
+++ update_templates.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget http://onsen.io/project-templates/gen/onsenui-master-detail.zip -O templates/onsen_master_detail.zip
-wget http://onsen.io/project-templates/gen/onsenui-sliding-menu.zip -O templates/onsen_sliding_menu.zip
-wget http://onsen.io/project-templates/gen/onsenui-tab-bar.zip -O templates/onsen_tab_bar.zip
-wget http://onsen.io/project-templates/gen/onsenui-split-view.zip -O templates/onsen_split_view.zip
+curl http://onsen.io/project-templates/gen/onsenui-master-detail.zip | gunzip > templates/onsen_master_detail.zip
+curl http://onsen.io/project-templates/gen/onsenui-sliding-menu.zip | gunzip > templates/onsen_sliding_menu.zip
+curl http://onsen.io/project-templates/gen/onsenui-tab-bar.zip | gunzip > templates/onsen_tab_bar.zip
+curl http://onsen.io/project-templates/gen/onsenui-split-view.zip | gunzip > templates/onsen_split_view.zip
