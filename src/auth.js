0c99748d826c576c7a3a2cdf0f9d7d9f184e9089
Masahiro Tanaka <masahiro@asial.co.jp>
2015-06-15 16:45:22 +0900

    Support alert, redirect and evaluation messages.

--- src/auth.js
+++ src/auth.js
@@ -4,6 +4,7 @@
   var read = require('read'),
     path = require('path'),
     Q = require('q'),
+    open = require('open'),
     Monaca = require('monaca-lib').Monaca,
     Localkit = require('monaca-lib').Localkit;
 
@@ -115,16 +116,47 @@
 
             monaca.login(credentials.email, credentials.password, {version: 'monaca-cli ' + pkg.version}).then(
               function() {
-                util.print('Successfully signed in as ' + credentials.email + '.');
+                var user = monaca.loginBody;
+                if (user.hasOwnProperty("localkitEvaluationDays")) {
+                  // Under evaluation period
+                  util.warn('Monaca CLI is under the evaluation period. It will expire in ' + user.localkitEvaluationDays + ' days.');
+                  util.warn('You need to upgrade the plan when the evaluation period ends.')
+                }
+                util.print('Successfully signed in as ' + user.username + '.');
               },
               function(error) {
                 if (error === 'ECONNRESET') {
-                  util.err('Unable to connect to Monaca Cloud. Are you connected to the internet?');
+                  util.print('Unable to connect to Monaca Cloud. Are you connected to the internet?').warn;
                   util.print('If you need to use a proxy, please configure it with "monaca proxy".');
                 }
                 else {
-                  util.err('Unable to sign in: ' + error);
-                  util.print('If you don\'t yet have a Monaca account, please sign up at https://monaca.mobi/en/register/start .');
+                  if (error.hasOwnProperty("code") && error.code == 503) {
+                    if (error.hasOwnProperty("result") && error.result.hasOwnProperty("confirm") && error.result.confirm) {
+                      util.warn(error.message);
+                      read({ prompt: ' [Y/n]:' }, function(err, answer) {
+                        if (answer.toLowerCase().charAt(0) !== 'n') {
+                          if (error.result.hasOwnProperty("redirect")) {
+                            open(error.result.redirect);
+                          }
+                        }
+                      });
+                    } else {
+                      util.warn(error.message);
+                      if (error.hasOwnProperty("result") && error.result.hasOwnProperty("redirect")) {
+                        read({ prompt: 'Press Enter to continue...' }, function() {
+                          open(error.result.redirect);
+                        });
+                      }
+                    }
+                  } else if (error.hasOwnProperty("code") && error.code == 402) {
+                    util.err("Your Monaca CLI evaluation period has expired. Please upgrade the plan to continue.");
+                    read({ prompt: 'Press Enter to display upgrade page.' }, function(err, answer) {
+                      open("https://monaca.mobi/plan/change");
+                    });
+                  } else {
+                    util.err('Unable to sign in: ' + error.message);
+                    util.print('If you don\'t yet have a Monaca account, please sign up at https://monaca.mobi/en/register/start .');
+                  }
                 }
               }
             );
