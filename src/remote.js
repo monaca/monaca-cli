5aad84337a59f2eeb2f4a706d5705fde851ab0bd
JW-Vinayak <vinayak.mote@Xoriant.com>
2015-08-11 15:47:42 +0530

    changed remote build command to call monaca.buildProject() method.

--- src/remote.js
+++ src/remote.js
@@ -19,18 +19,20 @@
     'remote build': {
       description: 'build project on Monaca Cloud',
       longDescription: [
-        'Build the project on Monaca Cloud.',
-        '',
-        'Don\'t forget to upload the latest project before building',
-        'using `monaca upload`.'
+        'Build the project on Monaca Cloud.'        
       ],
       usage: ['monaca remote build'],
       options: [
-        ['--debugger', 'build the custom debugger']
+        ['--platform', 'Should be one of - ios, android, windows'],
+        ['--buid-type', 'Should be one of - debug (for iOS, Android and Windows. It is default option.),'],
+        ['', 'test (for iOS only),'],
+        ['', 'release (for iOS, Android and Chrome Apps)'],
+        ['--android_webview', 'If platform is android. Should be one of - default, crosswalk'],
+        ['--android_arch', 'Required if --android_webview is crosswalk. Should be one of - x86, arm'] 
       ],
-      examples: [
-        'monaca remote build',
-        'monaca remote build --debugger'
+      examples: [        
+        'monaca remote build --platform=ios --build-type=test',
+        'monaca remote build --platform=android --build-type=debug --android_webview=crosswalk --android_arch=arm'
       ]
     }
   };
@@ -64,26 +66,41 @@
   };
 
   RemoteTask.prototype.build = function() {
-    monaca.getProjectId(process.cwd()).then(
-      function(projectId) {
-        var url = 'https://ide.monaca.mobi/project/' + projectId + '/' + (argv.debugger ? 'debugger' : 'build');
 
-        return monaca.getSessionUrl(url);
-      },
-      function(error) {
-        util.err('This is not a Monaca project.');
-        process.exit(1);
+    if (!argv.platform || argv['build-type']) {
+      util.err('"platform" and "build-type" are mandatory parameters.');      
+      process.exit(1);
+    }
+
+    var params = {
+      platform: argv.platform,
+      purpose: argv['build-type']
+    }
+
+    if(argv.android_webview) {
+      params.android_webview = argv.android_webview;
+    }
+
+    if(argv.android_arch) {
+      params.android_arch = argv.android_arch;
+    }
+
+    // using hardcoded project_id until a method is written to get project ids.
+    monaca.buildProject("5461dd0a7e2193d95ed41537", params).
+    then(function(result) {
+      if(result.binary_url) {
+        util.print("Url to download your package is " +  result.binary_url);
       }
-    )
-    .then(
-      function(url) {
-        open(url);
-      },
-      function(error) {
-        util.err('Unable to open build page.');
-        process.exit(1);
-      }
-    );
+      else {
+        util.err(result.error_message);
+      }      
+    },
+    function(err) {
+      util.err(err);
+    },
+    function (progress) {
+      util.print(progress);
+    });
   };
 
   exports.RemoteTask = RemoteTask;
