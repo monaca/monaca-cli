d433e074a3cda88dce91af93d80b580f386c545a
エリス <ellis.bryan@gmail.com>
2016-06-28 11:20:14 +0900

    Replaced http-server usage with webpack-dev-server.

--- src/serve.js
+++ src/serve.js
@@ -51,93 +51,32 @@ ServeTask.assureCordovaProject = function(projectPath) {
 };
 
 ServeTask.run = function(taskName) {
-  this.assureCordovaProject(process.cwd()).then(function(){
-    if (monaca.requireTranspile(process.cwd())) {
-      util.print('Running transpiler');
-    }
-    return monaca.transpile(process.cwd());
-  }).then(
-    function() {
-      var fixLog = function(data) {
-        return data.toString()
-          .split('\n')
-          .filter(function(item) {
-            return item !== '';
-          })
-          .map(function(item) {
-            return item + '\n';
-          });
-      };
+   this.assureCordovaProject(process.cwd()).then(function() {
+    process.env.NODE_PATH = monaca.USER_CORDOVA;
 
-      // Execute filewatcher here with transpiler
-      var FileWatcher = require(path.join(__dirname, '..', 'node_modules', 'monaca-lib', 'src', 'localkit', 'fileWatcher'));
-      var fileWatcherTranspiler = new FileWatcher();
+    var webpack = monaca.getWebPackModule();
+    var WebpackDevServer = monaca.getWebpackDevServer();
+    var config = monaca.getWebPackConfigs(process.cwd());
+    config.entry.unshift("webpack-dev-server/client?http://0.0.0.0:8080/");
+    config.plugins = [
+      new webpack.HotModuleReplacementPlugin()
+    ];
 
-      if (monaca.requireTranspile(process.cwd())) {
-        fileWatcherTranspiler.onchange(function(changeType, filePath) {
-          util.print('Transpiling ' + filePath + '...');
+    var compiler = webpack(config);
+    var server = new WebpackDevServer(compiler, {
+      hot: true,
+      compress: true,
+      quiet: false,
+      noInfo: false,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+      },
+      stats: { colors: true }
+    });
 
-          monaca.transpile(process.cwd())
-	    .then(function() {
-              util.print('Done');
-	    }, function(error) {
-	      util.err('Failed to transpile: ', error);
-	    });
-        }.bind(this));
-        util.print('Watching changes...');
-        fileWatcherTranspiler.run(path.join(process.cwd(), 'src'));
-      }
-
-      var processes = [{
-        name: 'http-server',
-        process: exec('node' + ' ' + path.join(__dirname, 'serve', 'node_modules', 'http-server', 'bin', 'http-server') + ' ' + path.join(process.cwd(), 'www') + ' -c-1 ' + (argv.open ? ' -o ' : '') + ' -p ' + (argv.port || 8000), {cwd: __dirname}),
-        color: 'cyan',
-        alive: true
-      }];
-
-      var stopProcesses = function() {
-        processes.forEach(function(item) {
-          item.process.kill();
-        });
-      };
-
-      processes.forEach(function(item) {
-        item.process.stdout.on('data', function(data) {
-          fixLog(data).forEach(function(log) {
-            process.stdout.write((item.name + ': ').bold[item.color] + log.info);
-          });
-        });
-
-        item.process.stderr.on('data', function(data) {
-          fixLog(data).forEach(function(log) {
-            process.stderr.write((item.name + ': ').bold[item.color] + log.error);
-          });
-        });
-
-        item.process.on('exit', function(code) {
-      	  item.alive = false;
-
-      	  var shouldKeepRunning = false;
-      	  processes.forEach(function(i) {
-            if (i.alive) {
-              shouldKeepRuninng = true;
-      	    }
-      	  });
-
-      	  if (!shouldKeepRunning) {
-            fileWatcherTranspiler.stop();
-      	  }
-
-          if (code !== 0) {
-            stopProcesses();
-            process.exit(code);
-          }
-        });
-      });
-    },
-    util.fail.bind(null, 'Failed serving project: ')
-  );
+    server.listen(8080, 'localhost', function(){});
+  });
 };
-
 module.exports = ServeTask;
 })();
