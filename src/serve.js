3f011e1fbcd4774c9204dc0620627695a5d57fa5
エリス <ellis.bryan@gmail.com>
2016-06-29 17:22:24 +0900

    Updated preview to use webpack-dev-server with user project configs and include http-server for non-transpilable projects.

--- src/serve.js
+++ src/serve.js
@@ -35,7 +35,9 @@ ServeTask.assureCordovaProject = function(projectPath) {
         } else {
           util.print('Installing packages. Please wait. This might take a couple of minutes.\n');
 
-          var npmProcess = exec('npm install --loglevel error', {cwd: path.join(__dirname, 'serve')});
+          var npmProcess = exec('npm install --loglevel error', {
+            cwd: path.join(__dirname, 'serve')
+          });
 
           npmProcess.stdout.on('data', util.print);
           npmProcess.stderr.on('data', util.err);
@@ -51,32 +53,90 @@ ServeTask.assureCordovaProject = function(projectPath) {
 };
 
 ServeTask.run = function(taskName) {
-   this.assureCordovaProject(process.cwd()).then(function() {
-    process.env.NODE_PATH = monaca.USER_CORDOVA;
+  this.assureCordovaProject(process.cwd()).then(
+      function() {
+        var fixLog = function(data) {
+          return data.toString()
+            .split('\n')
+            .filter(function(item) {
+              return item !== '';
+            })
+            .map(function(item) {
+              return item + '\n';
+            });
+        };
 
-    var webpack = monaca.getWebPackModule();
-    var WebpackDevServer = monaca.getWebpackDevServer();
-    var config = monaca.getWebPackConfigs(process.cwd());
-    config.entry.unshift("webpack-dev-server/client?http://0.0.0.0:8080/");
-    config.plugins = [
-      new webpack.HotModuleReplacementPlugin()
-    ];
+        var processes = [];
+        var bin;
+        if (monaca.requireTranspile(process.cwd())) {
+          // Webpack Route
+          bin = monaca.getWebpackDevServerBinPath();
 
-    var compiler = webpack(config);
-    var server = new WebpackDevServer(compiler, {
-      hot: true,
-      compress: true,
-      quiet: false,
-      noInfo: false,
-      watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000
+          processes.push({
+            name: 'webpack-dev-server',
+            process: exec('node' + ' ' + bin + ' --progress --config ' + path.join(process.cwd(), 'webpack.dev.config.js') +  (argv.port ? ' --port ' + argv.port : '')),
+            color: 'cyan',
+            alive: true
+          });
+
+        } else {
+          // HTTP Server Route
+          bin = path.join(__dirname, 'serve', 'node_modules', 'http-server', 'bin', 'http-server');
+
+
+          processes.push({
+            name: 'http-server',
+            process: exec('node' + ' ' + bin + ' ' + path.join(process.cwd(), 'www') + ' -c-1 ' + (argv.open ? ' -o ' : '') + ' -p ' + (argv.port || 8000), {
+              cwd: __dirname
+            }),
+            color: 'cyan',
+            alive: true
+          });
+        }
+
+        var stopProcesses = function() {
+          processes.forEach(function(item) {
+            item.process.kill();
+          });
+        };
+
+        processes.forEach(function(item) {
+          item.process.stdout.on('data', function(data) {
+            fixLog(data).forEach(function(log) {
+              process.stdout.write((item.name + ': ').bold[item.color] + log.info);
+            });
+          });
+
+          item.process.stderr.on('data', function(data) {
+            fixLog(data).forEach(function(log) {
+              process.stderr.write((item.name + ': ').bold[item.color] + log.error);
+            });
+          });
+
+          item.process.on('exit', function(code) {
+            item.alive = false;
+
+            var shouldKeepRunning = false;
+            processes.forEach(function(i) {
+              if (i.alive) {
+                shouldKeepRuninng = true;
+              }
+            });
+
+            if (!shouldKeepRunning) {
+              fileWatcherTranspiler.stop();
+            }
+
+            if (code !== 0) {
+              stopProcesses();
+              process.exit(code);
+            }
+          });
+        });
       },
-      stats: { colors: true }
-    });
+      util.fail.bind(null, 'Failed serving project: ')
+    );
+  };
 
-    server.listen(8080, 'localhost', function(){});
-  });
-};
-module.exports = ServeTask;
+  module.exports = ServeTask;
 })();
