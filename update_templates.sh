8b5cd80456cb92e933b57e4ff4101c58e2237663
Fran Dios <frankdiox@gmail.com>
2016-09-21 20:47:44 +0900

    Bump 2.1.3

--- update_templates.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl http://onsen.io/project-templates/gen/onsenui-master-detail.zip | gunzip > templates/onsen_master_detail.zip
-curl http://onsen.io/project-templates/gen/onsenui-sliding-menu.zip | gunzip > templates/onsen_sliding_menu.zip
-curl http://onsen.io/project-templates/gen/onsenui-tab-bar.zip | gunzip > templates/onsen_tab_bar.zip
-curl http://onsen.io/project-templates/gen/onsenui-split-view.zip | gunzip > templates/onsen_split_view.zip
