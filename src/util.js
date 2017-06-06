4048f713a481fe4a7b0e5f507ac0def4a0dfc94d
adamkozuch <adam.kozuch@gmail.com>
2017-06-06 17:13:43 +0900

    add methods for reading and writing monaca_cli_update.json

--- src/util.js
+++ src/util.js
@@ -1,7 +1,9 @@
 (function() {
 'use strict';
 
-var Q = require('q');
+var Q = require('q'),
+  fs = require('fs'),
+  path = require('path');
 
 var _print = function(type, items) {
   var msg = '';
@@ -162,6 +164,94 @@ var checkNodeRequirement = function() {
     printwarn('Please consider updating your Node to version 6 or higher.\n');
   }
 }
+var hasMonacaCLIUpdateFile = function(dir) {
+  var deferred = Q.defer();
+
+  var settingsFile = path.join(dir, 'monaca_cli_update.json');
+
+  fs.exists(settingsFile, function(exists) {
+    if (exists) {
+      deferred.resolve(settingsFile);
+    } else {
+      deferred.reject(settingsFile);
+    }
+  });
+
+  return deferred.promise;
+};
+
+var getCLIUpdate = function(dir, key) {
+  var deferred = Q.defer();
+
+  hasMonacaCLIUpdateFile(dir)
+    .then(function(settingsFile) {
+      fs.readFile(settingsFile, function(error, data) {
+        if (error) {
+          deferred.reject(error);
+        } else {
+          try {
+            var settings = JSON.parse(data.toString());
+            deferred.resolve(settings[key]);
+          }
+          catch (e) {
+            deferred.reject(e);
+          }
+        }
+      });
+    },
+      function(updateFile) {
+        var obj = {};
+
+        fs.writeFile(updateFile, JSON.stringify(obj), function(error) {
+          if (error) {
+            deferred.reject(error);
+          } else {
+            deferred.resolve();
+          }
+        });
+      }
+    );
+
+  return deferred.promise;
+};
+
+var setCLIUpdate = function(dir, key, value) {
+  var deferred = Q.defer();
+
+  hasMonacaCLIUpdateFile(dir)
+  .then(function(updateFile) {
+    fs.readFile(updateFile, function(error, data) {
+      try {
+        var obj = JSON.parse(data.toString());
+
+        obj[key] = value;
+
+        fs.writeFile(updateFile, JSON.stringify(obj, null, 2), function(error) {
+          if (error) {
+            deferred.reject(error);
+          } else {
+            deferred.resolve();
+          }
+        });
+      } catch (e) {
+        deferred.reject(e);
+      }
+    });
+    },
+    function(updateFile) {
+      var obj = {};
+      obj[key] = value;
+
+      fs.writeFile(updateFile, JSON.stringify(obj), function(error) {
+        if (error) {
+          deferred.reject(error);
+        } else {
+          deferred.resolve();
+        }
+      });
+    });
+  return deferred.promise;
+};
 
 module.exports = {
   print: println,
@@ -174,6 +264,8 @@ module.exports = {
   displayObjectKeys: displayObjectKeys,
   displayLoginErrors: displayLoginErrors,
   displayHelp: displayHelp,
-  checkNodeRequirement: checkNodeRequirement
+  checkNodeRequirement: checkNodeRequirement,
+  setCLIUpdate: setCLIUpdate,
+  getCLIUpdate: getCLIUpdate
 };
 })();
