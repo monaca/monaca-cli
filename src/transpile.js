10083425d2043fdd73ca9ad9a587649f6b0f292e
David Alvarez <davidac0291@gmail.com>
2018-07-12 19:03:53 +0900

    Changed transpiled and preview process getting  Webpack 4 config. Removed demo command.

--- src/transpile.js
+++ src/transpile.js
@@ -1,11 +1,9 @@
-var path = require('path'),
-  fs = require('fs'),
-  exec = require('child_process').exec,
-  util = require(path.join(__dirname, 'util')),
-  lib = require(path.join(__dirname, 'lib')),
-  Monaca = require('monaca-lib').Monaca;
+let path = require('path');
+let util = require(path.join(__dirname, 'util'));
+let lib = require(path.join(__dirname, 'lib'));
+let Monaca = require('monaca-lib').Monaca;
 
-var TranspileTask = {}, monaca;
+let TranspileTask = {}; let monaca;
 
 TranspileTask.run = function(taskName, info) {
   monaca = new Monaca(info);
@@ -13,61 +11,37 @@ TranspileTask.run = function(taskName, info) {
   lib.findProjectDir(process.cwd(), monaca)
   .then(
     function(dir) {
-      var projectDir = dir,
-        projectConfig;
+      let projectDir = dir;
 
-      try {
-        projectConfig = require(path.join(projectDir, 'package.json'));
-      } catch (err) {
-        return transpileProject(taskName, projectDir);
+      if (!monaca.isTranspilable(projectDir)) {
+        util.fail('This project is not transpilable.');
+      }
+    
+      let report = {
+        event: 'transpile'
+      };
+    
+      monaca.reportAnalytics(report);
+    
+      let isTranspileEnabled = monaca.isTranspileEnabled(projectDir);
+    
+      if (isTranspileEnabled) {
+        util.checkNodeRequirement();
       }
 
-      if (projectConfig && projectConfig.scripts && projectConfig.scripts.build) {
-        var childProcess = exec('npm run build');
+    return monaca.transpile(projectDir)
+      .then(
+        monaca.reportFinish.bind(monaca, report),
+        monaca.reportFail.bind(monaca, report)
+      )
+      .then(
+        util.success.bind(null),
+        util.fail.bind(null, 'Project has failed to transpile. ')
+      );
 
-        childProcess.stdout.on('data', function(data) {
-          process.stdout.write(data.toString());
-        });
-
-        childProcess.stderr.on('data', function(data) {
-          if (data) {
-            process.stderr.write(data.toString().error);
-          }
-        });
-      } else {
-        return transpileProject(taskName, projectDir);
-      }
     }
   )
   .catch(util.fail.bind(null, 'Project ' + taskName + ' failed: '));
-};
-
-var transpileProject = function(taskName, projectDir) {
-  if (!monaca.isTranspilable(projectDir)) {
-    util.fail('This project is not transpilable.');
-  }
-
-  var report = {
-    event: 'transpile'
-  };
-
-  monaca.reportAnalytics(report);
-
-  var isTranspileEnabled = monaca.isTranspileEnabled(projectDir);
-
-  if (isTranspileEnabled) {
-    util.checkNodeRequirement();
-  }
-
-  return monaca.transpile(projectDir)
-    .then(
-      monaca.reportFinish.bind(monaca, report),
-      monaca.reportFail.bind(monaca, report)
-    )
-    .then(
-      util.success.bind(null),
-      util.fail.bind(null, 'Project has failed to transpile. ')
-    );
 };
 
 module.exports = TranspileTask;
